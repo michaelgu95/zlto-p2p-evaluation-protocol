59121445c7371a6aaacbf5e8db5a596f440bd950
Michael Gu <michaelgu95@gmail.com>
2018-03-06 16:39:44 -0500

    Setup PM2 for AWS deploy hook

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'zlto_node',
+    script: './server.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ec2-user',
+      host: 'ec2-54-229-185-150.eu-west-1.compute.amazonaws.com',
+      key: '~/.ssh/node_eval_p2p.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:michaelgu95/p2pEvalNode.git',
+      path: '/home/ec2-user/zlto_node',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
